1fbbba93dae372da069ca4c80cc526af259fd004
aviflombaum <avi@flombaum.com>
2015-09-02 01:21:56 -0400

    consistent board

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -18,14 +18,17 @@ describe '/lib/display_board.rb' do
   it 'defines a method display_board' do
   end
 
+  # Better spec out the specifications of the ASCII board
+  # 3 space per cel
+  # 11 - per row break
   it 'the #display_board method prints out a tic tac toe board' do
     output = capture_puts{ display_board }
 
-    expect(output).to include("  |  |  ")
-    expect(output).to include("--------")
-    expect(output).to include("  |  |  ")
-    expect(output).to include("--------")
-    expect(output).to include("  |  |  ")
+    expect(output).to include("   |   |   ")
+    expect(output).to include("-----------")
+    expect(output).to include("   |   |   ")
+    expect(output).to include("-----------")
+    expect(output).to include("   |   |   ")
   end
 end
 
