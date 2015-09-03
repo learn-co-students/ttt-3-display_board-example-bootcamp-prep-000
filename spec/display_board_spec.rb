1bf5169526d80ead6868dfc0f38fc390840b5bc3
aviflombaum <avi@flombaum.com>
2015-09-02 22:11:12 -0400

    Initial Master

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -1,34 +1,43 @@
-require 'pry'
-
 require_relative '../lib/display_board'
-
-def capture_puts
-  begin
-    old_stdout = $stdout
-    $stdout = StringIO.new('','w')
-    yield
-    $stdout.string
-  ensure
-    $stdout = old_stdout
-  end
-end
-
 
 describe '/lib/display_board.rb' do
   it 'defines a method display_board' do
+    expect(defined?(display_board)).to be_truthy
   end
 
-  # Better spec out the specifications of the ASCII board
-  # 3 space per cel
-  # 11 - per row break
-  it 'the #display_board method prints out a tic tac toe board' do
-    output = capture_puts{ display_board }
+  context "#display_board method" do
+    it 'represents a cell as a string with 3 spaces' do
+      output = capture_puts{ display_board }
 
-    expect(output).to include("   |   |   ")
-    expect(output).to include("-----------")
-    expect(output).to include("   |   |   ")
-    expect(output).to include("-----------")
-    expect(output).to include("   |   |   ")
+      expect(output).to include("   ")
+    end
+    
+    it 'separates cells with a | character' do
+      output = capture_puts{ display_board }
+
+      expect(output).to include("   |   ")      
+    end
+
+    it 'prints an 3 cell row' do
+      output = capture_puts{ display_board }
+
+      expect(output).to include("   |   |  ")
+    end
+
+    it 'separates rows with a line of 11 -' do
+      output = capture_puts{ display_board }
+
+      expect(output).to include("-----------")
+    end
+
+    it 'prints a 3x3 tic tac toe board' do
+      output = capture_puts{ display_board }
+
+      expect(output).to include("   |   |   ")
+      expect(output).to include("-----------")
+      expect(output).to include("   |   |   ")
+      expect(output).to include("-----------")
+      expect(output).to include("   |   |   ")
+    end
   end
 end
-
