45edc68aabbb5a2148774ed8208b2f701de10ae7
Sam Marcus <sammarcus@users.noreply.github.com>
2015-10-07 23:49:41 -0400

    fixed merge conflict in solution

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,22 +1,8 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  # puts <<-DOC
-  #    |   |   
-  # -----------
-  #    |   |   
-  # ------------
-  #    |   |   
-  # DOC
-
-  puts "   |   |   
------------
-   |   |   
------------
-   |   |  "
-
-  # puts "   |   |   "
-  # puts "-----------"
-  # puts "   |   |   "
-  # puts "-----------"
-  # puts "   |   |   "
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
