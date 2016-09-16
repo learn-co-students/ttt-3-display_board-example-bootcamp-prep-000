94b8b5acc3c2300fb0ec09c1d58edd07eb9a4e15
NATHAN ROBBERT <admin@bigrocketgames.com>
2016-09-16 22:53:41 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+display_board
