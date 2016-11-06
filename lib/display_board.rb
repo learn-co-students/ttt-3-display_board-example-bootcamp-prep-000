6424300bf3e0c5cce892633b716af3b5ea21a705
Andrew Nease <andrew.nease.code@gmail.com>
2016-11-06 20:42:59 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,11 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+
+def display_board
+  empty = "   "
+  puts empty + "|" + empty + "|" + empty
+  puts "-----------"
+  puts empty + "|" + empty + "|" + empty
+  puts "-----------"
+  puts empty + "|" + empty + "|" + empty
+end
