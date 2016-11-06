c803bb2dd513d19ca24fd771cec38b6ce6cdc5b8
Noah Peller <napeller@gmail.com>
2016-11-06 19:40:40 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,18 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+
+board = Array.new(3) { Array.new(3) {" "} }
+
+def display_board(b = Array.new(3) { Array.new(3) {" "} })
+  cellsarray = [" " , " | " , " | " , " "]
+  lines = "-----------"
+  #interleave arrays
+  output_board = []
+  b.each do |l|
+    output_board.push(cellsarray.zip(l).flatten.compact)
+  end
+  puts output_board[0].join
+  puts lines
+  puts output_board[1].join
+  puts lines
+  puts output_board[2].join
+end
