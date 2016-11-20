7054a4f008a2a2bb0ab3ee49b79dc00a490ced4a
Robert La Plant <rlaplantjr@sbcglobal.net>
2016-11-20 02:35:05 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,11 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board()
+  row = ["   |   |   ","   |   |   ","   |   |   "]
+  line = "-----------"
+  puts(row[0])
+  puts(line)
+  puts(row[1])
+  puts(line)
+  puts(row[2])
+
+end 
