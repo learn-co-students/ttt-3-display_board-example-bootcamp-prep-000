1bb76f9025d76ec2f86bccf606d6d4e83c09beeb
Devon Weldon <devon.w.weldon@gmail.com>
2016-11-29 21:17:48 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  print "   |   |   \n"
+  11.times{ print "-" }
+  puts
+  print "   |   |   \n"
+  11.times{ print "-" }
+  puts
+  print "   |   |   \n"
+end 
