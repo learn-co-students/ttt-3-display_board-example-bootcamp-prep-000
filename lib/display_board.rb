bafd375cd948de72c234fca7e523c33c3d4df8c0
Kesean Woodhouse <kesean.woodhouse@gmail.com>
2017-03-23 20:52:23 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,7 @@
 def display_board
-  row = ["   ","|","   ","|","   ","\n"].join()
-  row_break = "-----------\n"
-  tic_tac = row
-  2.times do
-    tic_tac+=row_break
-    tic_tac+=row
-  end
-print tic_tac
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
