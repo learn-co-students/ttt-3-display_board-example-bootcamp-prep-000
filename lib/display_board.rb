9d21c811f511db0daa7d5c2656ff28f5e87dc9f2
tzvi seliger <tzviseliger@gmail.com>
2017-05-08 22:35:42 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,11 @@
+
+br = "\n"
+line_between="-----------"
+
+board_line1 ="   " + "|" + "   " +  "|" +"   "
+board_line2 ="   " + "|" + "   " +  "|" +"   "
+board_line3 ="   " + "|" + "   " +  "|" +"   "
+
+ttt_board_comp = board_line1+br+line_between+br+board_line2+br+line_between+br+board_line3
+puts ttt_board_comp
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
