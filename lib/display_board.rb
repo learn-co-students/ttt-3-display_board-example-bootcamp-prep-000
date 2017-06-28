64eeb1d9e224e874d270631a114fa94ea9e1c7f2
Joseph Moloughney <jmoloughney25@gmail.com>
2017-06-28 23:41:02 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,11 @@
-display_board = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
+board = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
+
 def display_board(board)
-  puts ("#{board[0]}|#{board[1]}|#{board[2]}")
-  puts ("-----------")
-  puts ("#{board[3]}|#{board[4]}|#{board[5]}")
-  puts ("-----------")
-  puts ("#{board[6]}|#{board[7]}|#{board[8]}")
+  row1 =  "#{board[0]}|#{board[1]}|#{board[2]}\n"
+  row2 =  "#{board[3]}|#{board[4]}|#{board[5]}\n"
+  row3 =  "#{board[6]}|#{board[7]}|#{board[8]}\n"
+  spacer = "-----------\n"
+  puts row1 + spacer + row2 + spacer + row3
 end
-print display_board(display_board)
+
+display_board(board)
