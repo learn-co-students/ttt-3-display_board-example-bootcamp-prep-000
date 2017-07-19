0828097e74534b69124009d2578571c81b9e4098
Nick Cianci <n.cianci3@gmail.com>
2017-07-19 03:36:29 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  board = Array.new(3) { Array.new(3, " ") }
+  board.each_with_index do |row, idx|
+    puts " #{row.join(' | ')} "
+    puts "-" * ((4 * row.length) - 1) unless idx == board.length - 1
+  end
+end
+
+display_board
