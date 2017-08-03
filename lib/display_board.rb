d3582bf2530251eb9b570cf77d624150571170ea
0000000 00000000 <xninfo2015@gmail.com>
2017-08-03 19:12:28 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,20 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def row
+  cell = %Q[   ]
+  (1..3).map do
+    cell
+  end.join("|")
+end
+def display_board
+  l = row.size
+  sep = "-"*(l)
+
+board =   (1..5).map {|i|
+    if i % 2 == 0
+      sep
+    else
+      row
+    end
+  }.join("\n")
+  puts board
+end
