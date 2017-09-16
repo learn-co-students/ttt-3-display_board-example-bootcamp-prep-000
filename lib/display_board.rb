427340ab57aeffb15b75b6f4300c79a79c208286
Anthony Fields <janthonyfields@gmail.com>
2017-09-16 03:58:57 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,12 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  horiz = "-----------"
+  three_space = "   "
+  # cells = puts "#{three_space}|#{three_space}|"
+  # line = puts "#{horiz}"
+  puts "#{three_space}|#{three_space}|#{three_space}"
+  puts "#{horiz}"
+  puts "#{three_space}|#{three_space}|#{three_space}"
+  puts "#{horiz}"
+  puts "#{three_space}|#{three_space}|#{three_space}"
+
+end
