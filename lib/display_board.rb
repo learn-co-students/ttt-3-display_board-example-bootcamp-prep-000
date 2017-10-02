f1e258e543a71f8781988af65d3961550cf4522e
Michael Wong <mik9080@gmail.com>
2017-10-02 21:25:41 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,11 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  cell="   "
+  row_sep="-"*11
+  row="#{cell}|#{cell}|#{cell}"
+  puts row
+  puts row_sep
+  puts row
+  puts row_sep
+  puts row
+end
