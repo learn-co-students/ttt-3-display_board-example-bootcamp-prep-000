9f48a98b6ae6118d20a75b7b167f5db58b841c99
MYKYTA ZAMIATIN <4406015@gmail.com>
2017-10-07 02:00:25 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,6 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+def display_board
+  phrase =  "   |   |   "  "-----------"
+  3.times { puts  phrase }
+
+end
--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -11,11 +11,11 @@ describe '/lib/display_board.rb' do
 
       expect(output).to include("   ")
     end
-    
+
     it 'separates cells with a | character' do
       output = capture_puts{ display_board }
 
-      expect(output).to include("   |   ")      
+      expect(output).to include("   |   ")
     end
 
     it 'prints an 3 cell row' do
