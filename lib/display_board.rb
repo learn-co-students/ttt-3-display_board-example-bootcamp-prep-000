3c766f9b12d387e4354ebbce99fc6a94feb55502
Jay Porta <jasonmatthewporta@gmail.com>
2017-10-29 12:04:14 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,20 +1,10 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
-$cells = ["      ", "   ", " | ", "   ", " | ", "   "]
-$line = "      ---------------"
-def print_cells
-  $i = 0
-  until $i == $cells.size do
-    print $cells[$i]
-    $i += 1
-  end
-  puts "\n#{$line}"
-end
-
+#Code to satisfy lesson requirements
 def display_board
-  puts $line
-  3.times do
-  print_cells
+puts "   |   |   \n"
+puts "-----------\n"
+puts "   |   |   \n"
+puts "-----------\n"
+puts "   |   |   \n"
 end
-end
-display_board
