e4ce9d3faee9d2d124b7bdb858ef24a0bbcadbb8
philip ka sing leung <philip.leung90@gmail.com>
2017-12-21 19:03:57 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,11 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+welcome = "Welcome To Tic Tac Toe"
+
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  "What move would X like to make (1-9)?"
+end
