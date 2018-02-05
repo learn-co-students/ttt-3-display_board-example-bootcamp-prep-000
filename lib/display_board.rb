2b2ce9cb3b632f79f7998f38cf6efd75d3323a3d
Felice Forby <feliceforby@gmail.com>
2018-02-05 23:02:07 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  cell = "   "
-  pipe = "|"
+  row = "   |   |   "
   divider = "-----------"
-  row = cell + pipe + cell + pipe + cell
   puts row
   puts divider
   puts row
