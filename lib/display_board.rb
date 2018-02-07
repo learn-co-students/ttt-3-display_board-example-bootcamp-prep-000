be86b6df837d249f8a9199ff0bf63b726ef5eb19
Aspen James <aspenjames@tqca.org>
2018-02-07 15:11:34 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,12 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  row = "   |   |   "
+  line = "-----------"
+  puts row
+  puts line
+  puts row
+  puts line
+  puts row
+end
+
+display_board
