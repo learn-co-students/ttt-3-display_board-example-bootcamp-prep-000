f1365646ddb6a32dc4424f9fb5db2bb8171924e9
Hamed Drame <hamedpolo62@gmail.com>
2018-02-20 22:23:41 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,12 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+
+it 'prints a 3x3 tic tac toe board' do
+  output = capture_puts{ display_board }
+  output_array = output.split "\n"
+
+   expect(output_array[0]).to include("   |   |   ")
+   expect(output_array[1]).to include("-----------")
+   expect(output_array[2]).to include("   |   |   ")
+   expect(output_array[3]).to include("-----------")
+   expect(output_array[4]).to include("   |   |   ")
+end
