a4f4fd96574ad8aebd152585f3bfda36739d1411
stayfo@gmail.com <stayfo@gmail.com>
2018-03-30 16:54:08 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,14 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  cell = "   "
+  pipe = "|"
+  lines = "-----------"
+  
+  puts "#{cell}#{pipe}#{cell}#{pipe}#{cell}"
+  puts "#{lines}"
+  puts "#{cell}#{pipe}#{cell}#{pipe}#{cell}"
+  puts "#{lines}"
+  puts "#{cell}#{pipe}#{cell}#{pipe}#{cell}"
+end
+
+display_board
