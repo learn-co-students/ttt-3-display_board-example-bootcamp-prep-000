dc08fab6d696c24d1356b24411416391f60366ef
katie.weber@powerhrg.com <katie.weber@powerhrg.com>
2018-04-19 17:02:16 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,12 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+row_1 = "   |   |   "
+divider  = "-----------"
+puts row_1
+puts divider
+puts row_1
+puts divider
+puts row_1
+end
+
+#display_board
