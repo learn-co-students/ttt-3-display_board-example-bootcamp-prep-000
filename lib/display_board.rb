03dcd1559a397c72b9c4874e6408e217b5631170
richardojo04@gmail.com <richardojo04@gmail.com>
2018-04-22 22:40:53 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,5 @@
 def display_board
-  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
