2509b8d74825dbf45acfc337a30a3bb67081aa11
Yazgylych Rahymov <y.rahymov@gmail.com>
2018-04-28 20:47:10 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,8 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  puts 2.times {puts "|" }
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
 end
