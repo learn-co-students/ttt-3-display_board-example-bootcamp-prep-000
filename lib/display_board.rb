3560f4e8d7ec6ce58c689a2115313cc98a888eb7
Joseph Milliken <joseph97milliken@gmail.com>
2018-05-03 19:33:55 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,17 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  board = Array.new(9, ' ')
+  line = 0
+
+  5.times do |i|
+    if i.odd?
+      puts '-----------'
+      next
+    end
+
+    board.slice(line, 3).each_with_index do |piece, piece_index|
+      print " #{piece} #{piece_index != 2 ? '|' : "\n"}"
+    end
+
+    line += 1
+  end
+end
