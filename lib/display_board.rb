76defab86e6f78f1aaf9e9a3af94b896de2d59d0
Nate Eckerson <nate.eckerson@gmail.com>
2018-06-09 21:12:07 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,15 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+
+
+def display_board
+  board = []
+  3.times do |i|
+  board << "   |   |   \n"  
+    if i/2 == 0
+      board << "-----------\n"
+    end
+  end
+  board.each do |x|
+    puts x
+  end
+end
