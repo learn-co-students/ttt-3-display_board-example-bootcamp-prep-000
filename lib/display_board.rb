f17d15818c89decaae83a99beac64df7357a232a
valentine-m@live.fr <valentine-m@live.fr>
2018-07-20 18:20:32 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+cell= "   "
+separation= "|"
+cell_row=cell+separation+cell+separation+cell
+line= "-----------"
+return puts cell_row, line, cell_row, line, cell_row
+end
+display_board
