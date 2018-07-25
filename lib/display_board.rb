a109020c7896230813f807e9a2b376ba6884bc0b
christleyjordan@gmail.com Christley <christleyjordan@gmail.com>
2018-07-25 07:59:02 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,20 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  print_row
+  print_row_seperator
+  print_row
+  print_row_seperator
+  print_row
+end
+
+def print_row
+  puts "   |   |   "
+end
+
+def print_row_seperator
+  puts "-----------"
+end
+
+def new_row
+  puts "\n"
+end
