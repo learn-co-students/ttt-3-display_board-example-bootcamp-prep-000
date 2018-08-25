e8b90a0fa8bbe1470c24c4b3fd83186484e58a04
Sachit Shetty <sachit.shetty@gmail.com>
2018-08-25 17:03:47 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,4 @@
 # n  |   |  "Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-  puts "   |   |  \n-----------\n   |   |  \n-----------\n   |   |  "
+  puts "   |   |   \n-----------\n   |   |   \n-----------\n   |   |   "
+end
