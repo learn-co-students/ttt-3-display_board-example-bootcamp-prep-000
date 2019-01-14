5b8cd202794fe147f07680798fc09305cc17e041
dsanalog <dsanalog@gmail.com>
2019-01-14 15:18:07 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,15 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def print_row
+  puts "   |   |   "
+end
+def print_seperator
+  puts "-----------"
+end
+
+def display_board
+  print_row
+  print_seperator
+  print_row
+  print_seperator
+  print_row
+end
