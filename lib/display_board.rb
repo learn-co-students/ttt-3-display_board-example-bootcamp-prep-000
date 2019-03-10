c945d413bc4811b119f4dc0bfe3a8c615de40124
Kevin Kamanda <kevkamanda@gmail.com>
2019-03-10 11:20:21 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,15 @@
+def display_board
+  #here we are defining the funcition
+  
+puts "   |   |   "  
+puts "-----------"
+puts "   |   |   "
+puts "-----------"
+puts "   |   |   "
+ end
+
+#this is calling the function to be returned 
+ display_board
+
+
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
