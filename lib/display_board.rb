21fa0abc8ff0ec8e11347288223cbd9c5fd831f7
Marco Frisina <marco@frisina.xyz>
2019-05-09 10:32:04 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,4 @@
-# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+# Define a method  display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
   puts "   |   |   "
   puts "-----------"
