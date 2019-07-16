6224d28804d62b574eda6774b3142e8aea59f4b1
Brian Wong <bwong92@gmail.com>
2019-07-16 14:03:08 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,1 +1,18 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
+def display_board
+  # output = "   |   |  "
+  # print output
+  # print "-----------"
+  # print output
+  # print "-----------"
+  # print output
+
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+
+puts display_board
